27f697044c2d2f97a57483a43e4328ad7462c6be
karmokardev <hridoykarmokar882@gmail.com>
2026-05-15 19:53:53 +0600

    Project Setup

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,6 @@
-import './bootstrap';
-
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+import axios from 'axios';
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
