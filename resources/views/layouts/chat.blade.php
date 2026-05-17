2bf69f2ff313a9345744daa9c071bcf09fbfb01e
karmokardev <hridoykarmokar882@gmail.com>
2026-05-17 22:52:00 +0600

    dark light

--- resources/views/layouts/chat.blade.php
+++ resources/views/layouts/chat.blade.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
-<html lang="en" x-data="{ darkMode: localStorage.getItem('darkMode') === 'true' }" x-bind:class="{ 'dark': darkMode }">
+<html lang="en"
+    x-data="{ darkMode: localStorage.getItem('darkMode') === 'true' }"
+    x-init="$watch('darkMode', val => localStorage.setItem('darkMode', val))"
+    :class="darkMode ? 'dark' : ''">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
